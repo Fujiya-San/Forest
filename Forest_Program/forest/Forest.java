bc9f7c8a77c86ca2cd480689255d5ed763dad4ed
shunsuke0819 <g1744951@cc.kyoto-su.ac.jp>
2019-06-02 19:43:39 +0900

    1.0.1

---
+++ Forest_Program/forest/Forest.java
@@ -0,0 +1,75 @@
+package forest;
+
+public class Forest extends Object {
+
+	private ArrayList<Node> nodes;
+
+	private ArrayList<Branch> branches;
+
+	private Rectangle bounds;
+
+	public void Forest() {
+
+	}
+
+	public void addBranch(Branch aBranch) {
+
+	}
+
+	public void addNode(Node aNode) {
+
+	}
+
+	public void arrange() {
+
+	}
+
+	public void arrange(SpiroModel aModel) {
+
+	}
+
+	protected Point arrange(Node aNode, Point aPoint, SpiroModel aModel) {
+		return null;
+	}
+
+	public Rectangle bounds() {
+		return null;
+	}
+
+	public void draw(Graphics aGraphics) {
+
+	}
+
+	public void flushBounds() {
+
+	}
+
+	protected void propagate(SpiroModel aModel) {
+
+	}
+
+	public ArrayList<Node> rootNodes() {
+		return null;
+	}
+
+	protected ArrayList<Node> sortNodes(ArrayList<Node> nodeCollection) {
+		return null;
+	}
+
+	public ArrayList<Node> subNodes(Node aNode) {
+		return null;
+	}
+
+	public ArrayList<Node> superNodes(Node aNode) {
+		return null;
+	}
+
+	public String toString() {
+		return null;
+	}
+
+	public Node whichOfNodes(Point aPoint) {
+		return null;
+	}
+
+}
---
+++ Forest_Program/forest/SpiroController.java
@@ -0,0 +1,13 @@
+package forest;
+
+public class SpiroController extends Controller {
+
+	public void SpiroController() {
+
+	}
+
+	public void mouseClicked(MouseEvent aMouseEvent) {
+
+	}
+
+}
