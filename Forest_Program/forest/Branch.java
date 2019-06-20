d6d9c77466213bf7b25a97ebc528903a7d2ebe86
shunsuke0819 <g1744951@cc.kyoto-su.ac.jp>
2019-06-20 18:40:02 +0900

    1.0.6

--- Forest_Program/forest/Branch.java
+++ Forest_Program/forest/Branch.java
@@ -23,11 +23,15 @@ public class Branch extends Object {
 	 * @param aGraphics 描画引数
 	 */
 	public void draw(Graphics aGraphics) {
-		Integer startX = this.start.getLocation().x + this.start.getExtent().x;
-		Integer startY = this.start.getLocation().y + (this.start.getExtent().y / 2);
-		Integer endX = this.start.getLocation().x;
-		Integer endY = this.end.getLocation().y + (this.end.getExtent().y / 2);
-		aGraphics.drawLine(startX, startY, endX, endY);
+		// Integer startX = this.start.getLocation().x + this.start.getExtent().x;
+		// Integer startY = this.start.getLocation().y + (this.start.getExtent().y / 2);
+		// Integer endX = this.start.getLocation().x;
+		// Integer endY = this.end.getLocation().y + (this.end.getExtent().y / 2);
+		double startX = this.start.getBounds().getX() + this.start.getBounds().getWidth();
+		double startY = this.start.getBounds().getY() + (this.start.getBounds().getHeight() / 2);
+		double endX = this.start.getBounds().getX();
+		double endY = this.end.getBounds().getY() + (this.end.getExtent().getY() / 2);
+		aGraphics.drawLine((int)startX, (int)startY, (int)endX, (int)endY);
 	}
 
 	/**
