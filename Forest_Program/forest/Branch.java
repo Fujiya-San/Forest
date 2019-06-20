51bff9c495fc7ab4a85eb55bb929eb4b28263ed7
藤山和紀 <fujiyamakazuki@FujiyanoMacBook.local>
2019-06-20 12:03:12 +0900

    1.0.5

--- Forest_Program/forest/Branch.java
+++ Forest_Program/forest/Branch.java
@@ -8,11 +8,20 @@ public class Branch extends Object {
 
 	private Node end;
 
+	/**
+	 * インスタンスを生成する。
+	 * @param from 元のノード(親)
+	 * @param to 新たなノード(子)
+	 */
 	public void Branch(Node from, Node to) {
 		this.start = from;
 		this.end = to;
 	}
 
+	/**
+	 * 描写メソッド
+	 * @param aGraphics 描画引数
+	 */
 	public void draw(Graphics aGraphics) {
 		Integer startX = this.start.getLocation().x + this.start.getExtent().x;
 		Integer startY = this.start.getLocation().y + (this.start.getExtent().y / 2);
@@ -21,10 +30,18 @@ public class Branch extends Object {
 		aGraphics.drawLine(startX, startY, endX, endY);
 	}
 
+	/**
+	 * 元ノードを応答
+	 * @return 元ノード
+	 */
 	public Node start() {
 		return this.start;
 	}
 
+	/**
+	 * 新たなノード
+	 * @return 新たなノード
+	 */
 	public Node end() {
 		return this.end;
 	}
