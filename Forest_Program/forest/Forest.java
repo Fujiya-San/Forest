87704693699b6f04357f1c042505336108ef1eb9
shunsuke0819 <g1744951@cc.kyoto-su.ac.jp>
2019-06-20 11:01:41 +0900

    1.0.5

--- Forest_Program/forest/Forest.java
+++ Forest_Program/forest/Forest.java
@@ -1,5 +1,6 @@
 package forest;
 
+import java.util.Objects;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.Rectangle;
@@ -15,15 +16,17 @@ public class Forest extends Object {
 	private Rectangle bounds;
 
 	public void Forest() {
-
+		nodes = new ArrayList<>();
+		branches = new ArrayList<>();
+		bounds = new Rectangle();
 	}
 
 	public void addBranch(Branch aBranch) {
-
+		branches.add(aBranch);
 	}
 
 	public void addNode(Node aNode) {
-
+		nodes.add(aNode);
 	}
 
 	public void arrange() {
@@ -34,48 +37,112 @@ public class Forest extends Object {
 
 	}
 
-	protected Point arrange(Node aNode, Point aPoint,  TreeModel aModel) {
+	protected Point arrange(Node aNode, Point aPoint,  TreeModel aModel) 
+	{
+		if(Objects.equals(aNode.getStatus(), Constants.Unvisited))
+		{
+			aNode.setLocation(aPoint.getX(), aPoint.getY());
+		}
+
+		this.propagate(aModel);
+
+		this.subNodes(aNode).forEach(node -> this.arrange(node, new Point(aPoint.getX()+Constants.Interval.getX(), aPoint.getY()+Constants.Interval.getY()), aModel));
+
+
 		return null;
 	}
 
-	public Rectangle bounds() {
+	public Rectangle bounds() 
+	{
+		return bounds;
+	}
+
+	public void draw(Graphics aGraphics) 
+	{
+		nodes.forEach(aNode -> aNode.draw(aGraphics));
+		branches.forEach(aBranch -> aBranch.draw(aGraphics));
+	}
+
+	public void flushBounds() 
+	{
+		bounds = new Rectangle();
+	}
+
+	protected void propagate(TreeModel aModel) 
+	{
+		aModel.changed();
+	}
+
+	public ArrayList<Node> rootNodes() 
+	{
+		ArrayList<Node> rootNodesList = new ArrayList<>();
+		Integer anIndex = 0;
+		for(Node aNode : nodes)
+		{
+			anIndex = 0 ;
+			for(Branch aBranch : branches)
+			{
+				if(Objects.equals(aNode, aBranch.end()))
+				{
+					break;
+				}else if(anIndex == branches.size()-1)
+				{
+					rootNodesList.add(aNode);
+				}
+				anIndex++;
+			}
+			
+		}
 		return null;
 	}
 
-	public void draw(Graphics aGraphics) {
-
+	protected ArrayList<Node> sortNodes(ArrayList<Node> nodeCollection) 
+	{
+		nodeCollection.sort( (node1, node2) -> node1.getName().compareTo(node2.getName()) );
+		return nodeCollection;
 	}
 
-	public void flushBounds() {
-
+	public ArrayList<Node> subNodes(Node aNode) 
+	{
+		ArrayList<Node> subNodesList = new ArrayList<>();
+		Branch[] branchesToSubNodes = (Branch[])(branches.stream().filter( aBranch -> Objects.equals(aBranch.start(), aNode) ).toArray());
+		for(Branch aBranch : branchesToSubNodes)
+		{
+			subNodesList.add(aBranch.end());
+		}
+		// for(Branch aBranch : branches)
+		// {
+		// 	if(Objects.equals(aBranch.start(), aNode))
+		// 	{
+		// 		subNodesList.add(aBranch.end());
+		// 	}
+		// }
+		return subNodesList;
 	}
 
-	protected void propagate(TreeModel aModel) {
-
-	}
-
-	public ArrayList<Node> rootNodes() {
-		return null;
-	}
-
-	protected ArrayList<Node> sortNodes(ArrayList<Node> nodeCollection) {
-		return null;
-	}
-
-	public ArrayList<Node> subNodes(Node aNode) {
-		return null;
-	}
-
-	public ArrayList<Node> superNodes(Node aNode) {
-		return null;
+	public ArrayList<Node> superNodes(Node aNode) 
+	{
+		ArrayList<Node> superNodesList = new ArrayList<>();
+		Branch[] branchesToSubNodes = (Branch[])(branches.stream().filter( aBranch -> Objects.equals(aBranch.end(), aNode) ).toArray());
+		for(Branch aBranch : branchesToSubNodes)
+		{
+			superNodesList.add(aBranch.start());
+		}
+		return superNodesList;
 	}
 
 	public String toString() {
 		return null;
 	}
 
-	public Node whichOfNodes(Point aPoint) {
-		return null;
+	public Node whichOfNodes(Point aPoint) 
+	{
+		return nodes.stream().filter(aNode -> aNode.getLocation().getX() < aPoint.getX() && 
+											 aNode.getLocation().getX()+aNode.getExtent().getX() > aPoint.getX() &&
+											 aNode.getLocation().getY() < aPoint.getY() &&
+											 aNode.getLocation().getY()+aNode.getExtent().getY() > aPoint.getY())
+							 .findFirst()
+							 .orElse(null);
 	}
 
 }
