ca54f84b4f31674465da1eabb74ed4bc76f1ff8a
shunsuke0819 <g1744951@cc.kyoto-su.ac.jp>
2019-06-20 18:39:25 +0900

    1.0.7

--- Forest_Program/forest/Node.java
+++ Forest_Program/forest/Node.java
@@ -20,69 +20,85 @@ public class Node extends Component
 	// private Integer status;
 	private Integer status;
 
+	private Rectangle bounds;
+
 	public void Node(String aString)
 	{
-
+		this.setName(aString);
 	}
 
 	public void draw(Graphics aGraphics)
 	{
-
+		aGraphics.drawRect((int)bounds.getX(), (int)bounds.getY(), (int)bounds.getWidth(), (int)bounds.getHeight());
+		aGraphics.drawString(this.name, (int)this.location.getX(), (int)this.location.getY());
 	}
 
 	public Rectangle getBounds()
 	{
-		return null;
+		return this.bounds;
 	}
 
 	public Point getExtent()
 	{
-		return null;
+		return this.extent;
 	}
 
 	public Point getLocation()
 	{
-		return null;
+		return this.location;
 	}
 
 	public String getName()
 	{
-		return null;
+		return this.name;
 	}
 
 	public Integer getStatus()
 	{
-		return null;
+		return this.status;
 	}
 
 	public void setExtent(Point aPoint)
 	{
-
+		this.extent = aPoint;
 	}
 
 	public void setLocation(Point aPoint)
 	{
-
+		this.location = aPoint;
 	}
 
 	public void setName(String aString)
 	{
-
+		this.name = aString;
 	}
 
 	public void setStatus(Integer anInteger)
 	{
+		this.status = anInteger;
+	}
 
+	public void setBounds()
+	{
+		Double x = new Double(this.location.getX()-Constants.Margin.getX());
+		Double y = new Double(this.location.getY()-Constants.Margin.getY());
+		Double width = new Double(this.extent.getX()+Constants.Margin.getX());
+		Double height = new Double(this.extent.getY()+Constants.Margin.getY());
+		// this.bounds = new Rectangle(this.location.getX()-Constants.Margin.getX(), 
+		// 							this.location.getY()-Constants.Margin.getY(), 
+		// 							this.extent.getX()+Constants.Margin.getX(), 
+		// 							this.extent.getY()+Constants.Margin.getY());
+		this.bounds = new Rectangle(x.intValue(), y.intValue(), width.intValue(), height.intValue());
 	}
 
 	protected int stringHeight(String string)
 	{
-		return 0;
+		return Constants.DefaultFont.getSize();
 	}
 
 	protected int stringWidth(String string)
 	{
-		return 0;
+		return Constants.DefaultFont.getSize()*string.length();
 	}
 
 	public String toString()
