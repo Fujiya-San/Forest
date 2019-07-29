2427c2e09323597d3c334f8824139b1a3b09edd4
悟j志 田原 <satorujshitahara@cclpc236.kyoto-su.ac.jp>
2019-07-29 10:52:43 +0900

    1.1.1

--- Forest_Program/forest/TreeView.java
+++ Forest_Program/forest/TreeView.java
@@ -13,17 +13,20 @@ public class TreeView extends View
 
 	/**
 	 * 情報を握っているTreeModelのインスタンスを束縛する。
+	 * 良好 (7月29日)
 	 */
 	protected TreeModel model;
 
 	/**
 	 * 制御を司るTreeControllerのインスタンスを束縛する。
+	 * 良好 (7月29日)
 	 */
 	protected TreeController controller;
 
 	/**
 	 * このクラスのインスタンスを生成するコンストラクタです。
 	 * @param  aModel モデル（Modelのインスタンス）
+	 * 良好 (7月29日)
 	 */
 	public TreeView(TreeModel aModel)
 	{
@@ -39,6 +42,7 @@ public class TreeView extends View
 	/**
 	 * このパネル（ビュー）の描画が必要になったときに動作するメソッドです。
 	 * @param aGraphics グラフィクス（描画コンテクスト）
+	 * 良好 (7月29日)
 	 */
 	public void paintComponent(Graphics aGraphics)
 	{
@@ -50,10 +54,11 @@ public class TreeView extends View
 	/**
 	 * スクロール量を指定された座標分だけ相対スクロールする。
 	 * @param aPoint X軸とY軸のスクロール量を表す座標
+	 * 良好 (7月29日)
 	 */
 	public void scrollBy2(Point aPoint)
 	{
-		
+
 		int x = aPoint.x;
 		int y = aPoint.y;
 		model.forest().moveBounds(new Point(x, y));
@@ -65,6 +70,7 @@ public class TreeView extends View
 	 * 指定された位置（座標）にノードが存在するかを調べるメソッドです。
 	 * @param  aPoint 位置（ビュー座標）
 	 * @return ノード、もしも見つからなかった場合には、nullを応答します。
+	 * 良好 (7月29日)
 	 */
 	public Node whichOfNodes(Point aPoint)
 	{
